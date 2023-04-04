fdd95e02bc1f75840c35225cb5f5b90997d83b0e
Bychkov Nikita Vladimirovich <94607989+nikbyk00@users.noreply.github.com>
2023-04-04 12:39:54 +0300

    Update auth.js

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const common = require('../common');
-const {User} = require('../model/user')
 const { doc } = require('../googleSheets')
 
 const router = express.Router()
@@ -10,18 +9,6 @@ router.post('/login', async (req, res) => {
     const reqData = req.body;
 
     try {
-
-            const user = await User.create({
-                    name: reqData.name,
-                    number: reqData.number,
-                    messenger: reqData.messenger,
-                    age: reqData.age,
-                    experience: reqData.experience,
-                    target: reqData.target,
-                    time: reqData.time,
-                    money: reqData.money,
-                    city: reqData.city
-            });
 
             await doc.loadInfo();
             const sheet = doc.sheetsByIndex[0];
